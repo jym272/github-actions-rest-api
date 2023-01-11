55397bf09595da690a5a1f66963fc6f3cbd60c0a
Jorge Clavijo <jym272@gmail.com>
2023-01-11 17:40:25 -0300

    build actions script

--- .github/actions/deploy-s3-js-action/setup.js
+++
@@ -1,9 +0,0 @@
-const exec = require('child_process').exec;
-exec('bash build.sh', (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return;
-  }
-  console.log(`${stdout}`);
-  console.log(`${stderr}`);
-});
